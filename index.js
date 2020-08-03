46d4d7fb4d9ba8e7dc7986b35771ac50aeca2017
Yego Allan <allanyego05@gmail.com>
2020-08-03 17:03:39 +0000

    welcome

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// Import stylesheets
-// import "./style.css";
-
-/**
- * Created with JetBrains WebStorm.
- * User: pwanwu
- * Date: 18/09/2013
- * Time: 17:41
- * To change this template use File | Settings | File Templates.
- */
-
 let questions = [];
 
 function loadQuestions(filename) {
