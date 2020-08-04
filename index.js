6374bb9de679da4d1f6d8e6115b8325600311623
yegow <allanyego05@gmail.com>
2020-08-04 14:55:39 +0300

    Add timing functionality

--- index.js
+++ index.js
@@ -9,89 +9,117 @@ function loadQuestions(filename) {
 var currentQuestion = 0;
 var correctAnswers = 0;
 var quizOver = false;
+let duration;
+let beatTimer = false;
 
 $(document).ready(async function () {
+  $(".questionsCard").hide();
+  $(".resultCard").hide();
+
   try {
-    questions = await loadQuestions("questions.json");
+    const test = await loadQuestions("questions.json");
+    questions = test.questions.map((q) => ({ ...q, picked: null }));
+    duration = test.duration;
   } catch {
     return;
   }
-  questions = questions.map((q) => ({ ...q, picked: null }));
 
+  $(".welcomeCard #duration").text(duration);
+  $(".welcomeCard #numQuestions").text(questions.length);
+  $(".welcomeCard button").on("click", startQuiz);
+});
+
+function startQuiz() {
+  $(".welcomeCard").hide();
+  const questionsCard = $(".questionsCard");
+  questionsCard.show();
   // Display the first question
   displayCurrentQuestion();
+  startTimer();
   // On clicking next, display the next question
-  $(this)
-    .find(".nextBtn")
-    .on("click", function () {
-      if (!quizOver) {
-        if (questions[currentQuestion].picked == null) {
-          const correctAnswer = questions[currentQuestion].correctAnswer;
-          const pickedOption = $("input[type='radio']:checked");
-          const correctOption = $(
-            `input[type='radio'][value='${correctAnswer}']`
+  questionsCard.find(".nextBtn").on("click", function () {
+    if (!quizOver) {
+      if (questions[currentQuestion].picked == null) {
+        const correctAnswer = questions[currentQuestion].correctAnswer;
+        const pickedOption = $("input[type='radio']:checked");
+        const correctOption = $(
+          `input[type='radio'][value='${correctAnswer}']`
+        );
+
+        const value = pickedOption.val();
+
+        if (value == undefined) {
+          displayAlert(
+            $(".quizMessage"),
+            "Please select an answer before proceeding!"
           );
-
-          const value = pickedOption.val();
-
-          if (value == undefined) {
-            displayAlert(
-              $(".quizMessage"),
-              "Please select an answer before proceeding!"
-            );
-          } else {
-            const nextBtn = $(".nextBtn");
-            const btnText = nextBtn.text();
-            nextBtn.attr("disabled", true).text("Moving...");
-
-            const card = pickedOption.parent().parent();
-            const correctCard = correctOption.parent().parent();
-            if (value == correctAnswer) {
-              card
-                .removeClass(["border-danger"])
-                .addClass(["border", "border-success"]);
-              correctAnswers++;
-            } else {
-              card.addClass(["border", "border-danger"]);
-              correctCard.addClass(["border", "border-success"]);
-            }
-            questions[currentQuestion].picked = Number(value);
-
-            setTimeout(() => {
-              currentQuestion++; // Since we have already displayed the first question on DOM ready
-              if (currentQuestion < questions.length) {
-                displayCurrentQuestion();
-              } else {
-                displayScore();
-                // Change the text in the next button to ask if user wants to play again
-                $(document).find(".nextButton").text("Play Again?");
-                quizOver = true;
-              }
-              nextBtn.attr("disabled", false);
-              nextBtn.text(btnText);
-            }, 3500);
-          }
         } else {
-          currentQuestion++;
-          displayCurrentQuestion();
+          const nextBtn = $(".nextBtn");
+          const btnText = nextBtn.text();
+          nextBtn.attr("disabled", true).text("Moving...");
+
+          const card = pickedOption.parent().parent();
+          const correctCard = correctOption.parent().parent();
+          if (value == correctAnswer) {
+            card
+              .removeClass(["border-danger"])
+              .addClass(["border", "border-success"]);
+            correctAnswers++;
+          } else {
+            card.addClass(["border", "border-danger"]);
+            correctCard.addClass(["border", "border-success"]);
+          }
+          questions[currentQuestion].picked = Number(value);
+
+          setTimeout(() => {
+            currentQuestion++; // Since we have already displayed the first question on DOM ready
+            if (currentQuestion < questions.length) {
+              displayCurrentQuestion();
+            } else {
+              beatTimer = true;
+              quizOver = true;
+              displayResultCard();
+            }
+            nextBtn.attr("disabled", false);
+            nextBtn.text(btnText);
+          }, 3500);
         }
       } else {
-        // quiz is over and clicked the next button (which now displays 'Play Again?'
-        quizOver = false;
-        $(document).find(".nextButton").text("Next Question");
-        resetQuiz();
+        currentQuestion++;
         displayCurrentQuestion();
-        hideScore();
       }
-    });
+    } else {
+      // quiz is over and clicked the next button (which now displays 'Play Again?'
+      // quizOver = false;
+      // $(document).find(".nextButton").text("Next Question");
+      // resetQuiz();
+      // displayCurrentQuestion();
+      // hideScore();
+    }
+  });
 
-  $(this)
-    .find(".previousBtn")
-    .on("click", () => {
-      currentQuestion--;
-      displayCurrentQuestion();
-    });
-});
+  questionsCard.find(".previousBtn").on("click", () => {
+    currentQuestion--;
+    displayCurrentQuestion();
+  });
+}
+
+function displayResultCard() {
+  $(".questionsCard").hide();
+  const resultCard = $(".resultCard");
+  resultCard.show();
+  // Display alert if user timed out
+  if (!beatTimer) {
+    $(`<div class="alert alert-warning">
+  Time is up
+  </div>`).prependTo(resultCard.find(".card-body"));
+  }
+  resultCard.find("#correct").text(correctAnswers);
+  resultCard.find("#total").text(questions.length);
+  resultCard
+    .find("#attempted")
+    .text(questions.filter((q) => q.picked != null).length);
+}
 
 // This displays the current question AND the choices
 function displayCurrentQuestion() {
@@ -148,21 +176,40 @@ function displayCurrentQuestion() {
   }
 }
 
-function resetQuiz() {
-  currentQuestion = 0;
-  correctAnswers = 0;
-  hideScore();
-}
+function startTimer() {
+  let now = new Date();
+  now.setMinutes(now.getMinutes() + duration);
+  const endTime = now.getTime();
 
-function displayScore() {
-  $(document)
-    .find(".quizContainer > .result")
-    .text("You scored: " + correctAnswers + " out of: " + questions.length);
-  $(document).find(".quizContainer > .result").show();
-}
-
-function hideScore() {
-  $(document).find(".result").hide();
+  const ticker = setInterval(() => {
+    now = Date.now();
+    const distance = endTime - now;
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const timer = $(".questionsCard .timer");
+    timer.text(`${minutes}m ${seconds}s`);
+    if (minutes === 0 && seconds < 50) {
+      timer.removeClass("bg-dark").addClass("bg-danger");
+      timer.animate(
+        {
+          opacity: 0.8,
+        },
+        {
+          complete: function () {
+            $(this).animate({
+              opacity: 1,
+            });
+          },
+        }
+      );
+    }
+    if (distance < 0) {
+      beatTimer = false;
+      quizOver = true;
+      clearInterval(ticker);
+      displayResultCard();
+    }
+  }, 1000);
 }
 
 function displayAlert(parent, msg, type = "danger") {
